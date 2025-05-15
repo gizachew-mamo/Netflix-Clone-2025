439abe8e5117891f824ddf2e2c82e4521f001d65
gizachew-mamo <Gizeworknewu@gmail.com>
2025-05-15 13:47:33 -0700

    fotter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-outer-container">
+      <div className="footer-inner-container">
+        <div className="footer-icons">
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="footer-data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relation</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms Of Use</li>
+              <li>corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service-code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy-write">&copy;1997-2025 Netflix,Inc.</div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
